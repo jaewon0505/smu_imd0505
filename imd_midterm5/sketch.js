ac44e7e1e6c0404070abf2de9cba92d8d45f9a3a
jaewon0505 <114197009+jaewon0505@users.noreply.github.com>
2022-11-11 09:13:14 +0900

    ok

--- imd_midterm5/sketch.js
+++ imd_midterm5/sketch.js
@@ -1,16 +1,16 @@
 let x = 100,
     y = 100,
     angle1 = 0.0,
-    segLength = 50;
+    segLength = 40;
 
 function setup() {
     createCanvas(710, 400);
-    strokeWeight(20.0);
-    stroke(51, 255, 51,);
+    strokeWeight(20.30);
+    stroke(51, 255, 51, 100);
 }
 
 function draw() {
-    background(0);
+    background(153, 0, 153);
 
     dx = mouseX - x;
     dy = mouseY - y;
@@ -19,7 +19,7 @@ function draw() {
     y = mouseY - sin(angle1) * segLength;
 
     segment(x, y, angle1);
-    ellipse(x, y, 20, 20);
+    ellipse(x, y, 30, 30);
 }
 
 function segment(x, y, a) {
